82727c8a8debd654b18506f1af986e41147bb26f
Miguel <miguelapb35@gmail.com>
2021-12-16 15:47:17 +0100

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "b45200de90msh80b417cd625de8fp15edebjsnc8490afa9e38",
+      "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
 
